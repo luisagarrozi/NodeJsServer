6a61a41644ce6456c01f0576ee2f4035dd8e2ece
luisa.oliveira <luisa.oliveira@trinto.com.br>
2022-06-06 08:38:31 -0300

    query params - part 1

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const http = require("http");
+const { URL } = require('url');
 
 const routes = require("./routes");
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  const parsedUrl = new URL('http://localhost:3000/users?order=asc');
+
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
 
   // Veriy if the route is in routes:
   const route = routes.find(
     (routeObj) => (
-      routeObj.method === request.method && routeObj.endpoint === request.url
+      routeObj.method === request.method && routeObj.endpoint === parsedUrl.pathname
   ));
 
   if (route) {
+    // Transform the iterable object into a javascript object:
+    request.query = Object.fromEntries(parsedUrl.searchParams)
     console.log('achou route')
     //If the route exists (a.k.a is in the routes module)
     route.handler(request, response);
   } else {
     // If the route does not exist, gives a warning
     response.writeHead(404, { "Content-Type": "text/html" });
-    response.end(`Cannot ${request.method} ${request.url}`);
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
   }
 });
 
