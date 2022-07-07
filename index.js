a97547f56f53460e5545f4a5c69c4cb67e352f80
luisa garrozi <luisagarrozi@gmail.com>
2022-07-06 21:34:05 -0300

    Version 1.0

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
 const http = require("http");
-const { URL } = require('url');
+const url = require("url");
+
+const bodyParser = require("./helpers/bodyParser");
 
 const routes = require("./routes");
 
 const server = http.createServer((request, response) => {
-  const parsedUrl = new URL('http://localhost:3000/users?order=asc');
+  const parsedUrl = url.parse(request.url, true);
 
-  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
+  console.log(
+    `Request Method: ${request.method} | Endpoint: ${parsedUrl.pathname}`
+  );
+
+  let { pathname } = parsedUrl;
+  let id = null;
+  // Divide the url at the / and then filter to remove empty space
+  const splitEndpoint = pathname.split("/").filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
 
   // Veriy if the route is in routes:
   const route = routes.find(
-    (routeObj) => (
-      routeObj.method === request.method && routeObj.endpoint === parsedUrl.pathname
-  ));
+    (routeObject) => (
+      routeObject.endpoint === pathname && routeObject.method === request.method
+));
 
   if (route) {
     // Transform the iterable object into a javascript object:
-    request.query = Object.fromEntries(parsedUrl.searchParams)
-    console.log('achou route')
+    request.query = parsedUrl.query;
+    request.params = { id };
     //If the route exists (a.k.a is in the routes module)
-    route.handler(request, response);
-  } else {
+    response.send = (status, body) => {
+      response.writeHead(status, { "Content-Type": "text/html" });
+      response.end(JSON.stringify(body));
+    };
+
+    if (["POST", "PUT", "PATCH"].includes(request.method)) {
+      bodyParser(request, () => route.handler(request, response));
+    } else {
+      route.handler(request, response);
+    }
+
     // If the route does not exist, gives a warning
+  } else {
     response.writeHead(404, { "Content-Type": "text/html" });
-    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
+    response.end(`Cannot ${request.method} ${pathname}`);
   }
 });
 
 // Basic listener to know if server started ok
-server.listen(3000, () => console.log('Server is running'))
+server.listen(3000, () => console.log('Server started at http://localhost:3000'))
